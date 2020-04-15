b6c9506636747c65cca945d981a0fa135e419789
Myron <dubtwee@gmail.com>
2020-04-15 09:16:20 -0500

    simplifying for lecture

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        React with Material-UI
+      </header>
+    );
+  }
+}
+
+export default Header;
